b03688ab07db50b1f21cf1e215bade6f2504e850
K K <kmk@gmx.de>
2022-01-12 19:35:17 +0100

    Überflüssige Codeschnipsel gelöscht

--- Assets/Scripts/Level1PlayerController.cs
+++ Assets/Scripts/Level1PlayerController.cs
@@ -22,35 +22,9 @@ public class Level1PlayerController : MonoBehaviour
             }                
         }
     }
-    [SerializeField]
-    private bool isBoosting = false;
-    public bool IsBoosting
-    {
-        get { return isBoosting; }
-        set
-        {
-            isBoosting = value;
-            if (isBoosting == true)
-            {
-                boost.enabled = true;
-                boostTime = boostTimeLimit;
-            }
-            else
-            {
-                boost.enabled = false;
-            }
-
-        }
-    }
     SpriteRenderer fire;
-    SpriteRenderer boost;
     Rigidbody2D rigid;
-    bool jump = false;
     bool destroyed = false;
-    float jumpTime = 0f;
-    float jumpTimeLimit = 0.5f;
-    float boostTime = 1.0f;
-    float boostTimeLimit = 1.0f;
 
     bool goingLeft = false;
     bool goingRight = false;
@@ -70,28 +44,13 @@ public class Level1PlayerController : MonoBehaviour
         {
             if (ts.name == "Fire")
                 fire = ts.gameObject.GetComponent<SpriteRenderer>();
-            if (ts.name == "Boost")
-                boost = ts.gameObject.GetComponent<SpriteRenderer>();
         }
         fire.enabled = false;
-        boost.enabled = false;
         //fire = GetComponentInChildren<>
     }
 
-    // Update is called once per frame
     void Update()
     {
-        /* // wird nicht verwendet -> kein Flappy Bird
-        if(Input.GetKeyDown(KeyCode.Space))
-        {
-            jump = true;
-            IsBoosting = true;
-        }
-        if(jumpTime > 0)
-        {
-            jumpTime -= Time.deltaTime;
-        }
-        */
         if (Input.GetKeyDown(KeyCode.LeftArrow))
             goingLeft = true;
         if (Input.GetKeyUp(KeyCode.LeftArrow))
@@ -121,28 +80,5 @@ public class Level1PlayerController : MonoBehaviour
             rigid.velocity = new Vector2(rigid.velocity.x, -verticalSpeed + 0.5f);
         if (!goingDown && goingUp)
             rigid.velocity = new Vector2(rigid.velocity.x, verticalSpeed + 0.5f);
-
-        /* // wird nicht verwendet -> kein Flappy Bird
-        if(IsBoosting)
-        {
-            boostTime -= Time.deltaTime;
-            if (boostTime < 0)
-            {
-                IsBoosting = false;
-                Debug.Log("Burning vorbei");
-            }
-        }
-        if(jumpTime <= 0)
-        {
-            rigid.velocity = new Vector2(1f, -0.6f);
-        }
-        if(jump)
-        {
-            Debug.Log("Junp");
-            jumpTime = jumpTimeLimit;
-            rigid.AddForce(new Vector2(0.0f, 4.8f), ForceMode2D.Impulse);
-            jump = false;
-        }
-        */
     }
 }

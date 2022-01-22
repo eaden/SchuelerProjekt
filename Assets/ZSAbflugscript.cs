9cb0b07274e298aa2138152a93a7219ba128a925
K K <kmk@gmx.de>
2022-01-22 12:25:25 +0100

    Alle Bilder + Zwischenszene Abflug

---
+++ Assets/ZSAbflugscript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZSAbflugscript : MonoBehaviour
+{
+    float timer = 0; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        gameObject.transform.Translate(Vector3.right * Time.deltaTime * 10);
+        /*
+        if (gameObject.transform.localScale.x < 10f)
+        {
+            timer = (Time.deltaTime);
+            gameObject.transform.localScale += new Vector3(timer, timer, timer);
+            //gameObject.transform.Translate(Vector3.right * Time.deltaTime/2);
+            if (gameObject.transform.localScale.x > 5f)
+            {
+                gameObject.transform.Translate(Vector3.right * Time.deltaTime*2);
+            }
+        }
+        else
+        {
+            
+        }
+        */
+    }
+}

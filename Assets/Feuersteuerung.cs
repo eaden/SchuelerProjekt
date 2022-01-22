405da57cfd28d269f594287a80876b053d81b9f2
K K <kmk@gmx.de>
2022-01-22 18:48:57 +0100

    Grundaufbau ZwischenSzene Start

---
+++ Assets/Feuersteuerung.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Feuersteuerung : MonoBehaviour
+{
+    List<SpriteRenderer> rendererListe = new List<SpriteRenderer>();
+    int rendererCount = 0;
+    int currentRenderer = 0;
+    int direction = 1;
+    float zaehler = 0.05f;
+    float zaehlerStartpunkt = 0.05f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        for(int i = 0; i < transform.childCount; i++)
+        {
+            GameObject aktuellesKind = transform.GetChild(i).gameObject;
+            if (aktuellesKind.name.Contains("Feuer"))
+                rendererListe.Add(aktuellesKind.GetComponent<SpriteRenderer>());
+        }
+        rendererCount = rendererListe.Count;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (rendererCount > 0)
+        {
+            zaehler -= Time.deltaTime;
+            if(zaehler < 0)
+            {
+                zaehler = zaehlerStartpunkt;
+                rendererListe[currentRenderer].enabled = false;
+                if ((currentRenderer == 0 && direction < 0) || currentRenderer == rendererCount - 1)
+                    direction *= -1;
+                currentRenderer += direction;
+                rendererListe[currentRenderer].enabled = true;
+
+            }
+        }
+    }
+}

901ddcb24df747aafe91a99606ba98efd1039ffb
K K <kmk@gmx.de>
2022-01-24 16:18:12 +0100

    Sterngenerator für Flugszene angepasst

--- Assets/SternGenerator.cs
+++ Assets/SternGenerator.cs
@@ -17,22 +17,26 @@ public class SternGenerator : MonoBehaviour
 
     void NeuerStern()
     {
-        aktuellerStern = (Instantiate(sternSammlung[Random.Range(0,sternSammlung.Length)], transform.position + new Vector3(0, Random.Range(0f, 5f) / 10f - 0.25f), Quaternion.identity));
+        aktuellerStern = (Instantiate(sternSammlung[Random.Range(0, sternSammlung.Length)], transform.position + new Vector3(Random.Range(0f, 5f) / 10f - 0.25f, Random.Range(0f, 5f) / 10f - 0.25f), transform.rotation));
         aktuelleGeschwindigkeit = startGeschwindigkeit + Random.Range(0, 4) - 3;
+        if(transform.parent.gameObject.name == "SterngeneratorFlugSzene")
+        {
+            aktuelleGeschwindigkeit = 0f;//(startGeschwindigkeit/4 + Random.Range(0, 4)/4 - 3/4)*-1;
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
         Vector3 pos = Camera.main.WorldToViewportPoint(aktuellerStern.transform.position);
-        if (pos.x > 1f)
+        if (pos.x > 1f || pos.y < 0)
         {
             Destroy(aktuellerStern);
             NeuerStern();
         }
         if (aktuellerStern != null)
         {
-            aktuellerStern.transform.Translate(new Vector3(aktuelleGeschwindigkeit*Time.deltaTime, 0, 0));
+            aktuellerStern.transform.Translate(Vector3.right * Time.deltaTime * aktuelleGeschwindigkeit);
         }
     }
 }
